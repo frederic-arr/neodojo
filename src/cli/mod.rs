605367db904d963ff2ea2d41143f39f8fb43dab5
Frédéric Arroyo <71824723+frederic-arr@users.noreply.github.com>
2024-05-15 19:08:48 +0200

    style: put struct at top of the file

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -6,6 +6,77 @@ use clap_complete::{generate, Generator};
 use itertools::Itertools;
 use std::path::Path;
 
+#[derive(Debug, Parser)]
+#[command(version, name = "neodojo")]
+#[command(about = "A better Dojo CLI")]
+#[command(after_help = "This is an experimental CLI tool, use at your own risk.")]
+#[command(styles=STYLES, help_template = help_template("help", false, true, true))]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+
+    #[command(flatten)]
+    pub verbose: clap_verbosity_flag::Verbosity,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Run the exercise test suite
+    #[command(subcommand_help_heading = "Exercise")]
+    #[command(styles=STYLES, help_template = help_template("command", false, true, false))]
+    Test {
+        /// Path to the exercise directory
+        #[arg(short, long, value_hint = ValueHint::DirPath)]
+        #[clap(short, long, default_value = "./")]
+        path: Box<Path>,
+
+        /// Watch for changes and re-run tests automatically
+        #[clap(short, long)]
+        watch: bool,
+
+        /// Show the raw output from the test runner
+        #[clap(short, long)]
+        raw: bool,
+
+        #[clap(short, long, default_values_t = Vec::<String>::default())]
+        filter: Vec<String>,
+    },
+
+    /// Upgrade neodojo to the latest version
+    #[command(subcommand_help_heading = "Misc")]
+    #[command(styles=STYLES, help_template = help_template("command", false, true, false))]
+    Upgrade {
+        /// Only check for updates
+        #[arg(short, long)]
+        check: bool,
+    },
+
+    /// Generate shell completions
+    #[command(styles=STYLES, help_template = help_template("command", true, false, false))]
+    Completion {
+        #[arg(value_enum)]
+        shell: clap_complete::Shell,
+    },
+}
+
+impl Cli {
+    pub fn exec(&self) {
+        let mut cmd = Cli::command();
+        match &self.command {
+            Commands::Test { path, filter, .. } => {
+                test::command(path, filter);
+            }
+            Commands::Completion { shell } => print_completions(*shell, &mut cmd),
+            Commands::Upgrade { check } => {
+                println!("Checking for updates...");
+                if *check {
+                }
+            }
+        }
+    }
+}
+
+
 const HEADER_STYLE: Style = Style::new().bold().fg_color(Some(Ansi(AnsiColor::Green)));
 const STYLES: Styles = Styles::styled()
     .literal(AnsiColor::BrightCyan.on_default().bold())
@@ -62,73 +133,6 @@ neodojo v{{version}}
     }
 }
 
-#[derive(Debug, Parser)]
-#[command(version, name = "neodojo")]
-#[command(about = "A better Dojo CLI")]
-#[command(after_help = "This is an experimental CLI tool, use at your own risk.")]
-#[command(styles=STYLES, help_template = help_template("help", false, true, true))]
-pub struct Cli {
-    #[command(subcommand)]
-    pub command: Commands,
-
-    #[command(flatten)]
-    pub verbose: clap_verbosity_flag::Verbosity,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum Commands {
-    /// Run the exercise test suite
-    #[command(subcommand_help_heading = "Exercise")]
-    #[command(styles=STYLES, help_template = help_template("command", false, true, false))]
-    Test {
-        /// Path to the exercise directory
-        #[arg(short, long, value_hint = ValueHint::DirPath)]
-        #[clap(short, long, default_value = "./")]
-        path: Box<Path>,
-
-        /// Watch for changes and re-run tests automatically
-        #[clap(short, long)]
-        watch: bool,
-
-        /// Show the raw output from the test runner
-        #[clap(short, long)]
-        raw: bool,
-    },
-
-    /// Upgrade neodojo to the latest version
-    #[command(subcommand_help_heading = "Misc")]
-    #[command(styles=STYLES, help_template = help_template("command", false, true, false))]
-    Upgrade {
-        /// Only check for updates
-        #[arg(short, long)]
-        check: bool,
-    },
-
-    /// Generate shell completions
-    #[command(styles=STYLES, help_template = help_template("command", true, false, false))]
-    Completion {
-        #[arg(value_enum)]
-        shell: clap_complete::Shell,
-    },
-}
-
 fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut std::io::stdout());
-}
-
-impl Cli {
-    pub fn exec(&self) {
-        let mut cmd = Cli::command();
-        match &self.command {
-            Commands::Test { path, .. } => {
-                test::command(path);
-            }
-            Commands::Completion { shell } => print_completions(*shell, &mut cmd),
-            Commands::Upgrade { check } => {
-                println!("Checking for updates...");
-                if *check {
-                }
-            }
-        }
-    }
 }
